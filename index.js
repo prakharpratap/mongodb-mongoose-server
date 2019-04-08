552c0f6543294f84d286045f93cd41265748f113
Prakhar Pratap Singh <prakhar0401@gmail.com>
2019-04-08 23:28:18 +0530

    final commit

--- index.js
+++ index.js
@@ -4,20 +4,45 @@ const Dish_model=require('./models/dishes');
 const url='mongodb://localhost:27017/conFusion';
 const connect=mongoose.connect(url);
 
-connect.then(function(db){
+connect.then(function(db) {
     console.log('connected successfully');
-   const newDish=Dish_model({name:"maggi",description:"test"});
-   newDish.save()
-       .then(function(dish){
-           console.log(dish);
-           return Dish_model.find({}).exec();
-       })
-       .then(function(d3ish){
-           console.log(d3ish);
-           return Dish_model.remove({});
-       })
-       .then(function(){
-           return mongoose.connection.close();
-       })
-       .catch(function(err){console.log(err);})
-});
+
+
+    Dish_model.create({
+        name: 'Uthappizza',
+        description: 'test'
+    })
+        .then((dish) => {
+            console.log(dish);
+
+            return Dish_model.findByIdAndUpdate(dish._id, {
+                $set: {description: 'Updated test'}
+            }, {
+                new: true
+            })
+                .exec();
+        })
+        .then((dish) => {
+            console.log(dish);
+
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Leonardo di Carpaccio'
+            });
+
+            return dish.save();
+        })
+        .then((dish) => {
+            console.log(dish);
+
+            return Dish_model.remove({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
+
